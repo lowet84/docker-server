7045c9c9f8df3a7f1c5f4abf84ca32cedbd457aa
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2018-05-21 14:23:37 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.

--- apps/registry/setup.sh
+++ apps/registry/setup.sh
@@ -1,4 +1,6 @@
-#/bin/bash
+#/bin/sh
 PASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1) 
-PATH=$(cat .env | grep SETTINGS_PATH) 
-echo $PATH
+SETTINGS=$(cat .env | grep "SETTINGS_PATH" | sed -e "s/SETTINGS_PATH=//g" ) 
+mkdir -p $SETTINGS/registry/auth
+docker run --entrypoint htpasswd registry:2 -Bbn default $PASSWD > $SETTINGS/registry/auth/htpasswd
+echo "Password is: $PASSWD"
