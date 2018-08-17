b697a708fb398f2d6a4a33fdb20c7fcc48b3e4df
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2018-08-17 16:56:06 +0200

    [Insert your commit message here. Be sure to make it descriptive.]

--- apps/gogs/backup/backup.sh
+++ apps/gogs/backup/backup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-docker exec -i gogs_gogs_1 sh -c 'cd .. && USER=git ./gogs backup --target /backup/'
+while true; do 
+  docker exec -i gogs_gogs_1 sh -c 'cd .. && USER=git ./gogs backup --target /backup/'
 
-PARENT=$(gdrive list -q "name contains 'GogsBackup'" --no-header | cut -d" " -f 1)
-FILE=$(ls -tr /backup | grep gogs | tail -1 )
-echo $FILE
-gdrive upload --parent $PARENT /backup/$FILE
+  PARENT=$(gdrive list -q "name contains 'GogsBackup'" --no-header | cut -d" " -f 1)
+  FILE=$(ls -tr /backup | grep gogs | tail -1 )
+  echo $FILE
+  gdrive upload --parent $PARENT /backup/$FILE  
+
+  sleep 24h
+done
+
