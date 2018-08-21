af3a6936da7db994bf381271be8b4d2d0bbf29c2
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2018-08-21 11:29:09 +0200

    Replace all whitespaces with tabs.

--- apps/registry/setup.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-PASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1) 
-SETTINGS=$(cat .env | grep "SETTINGS_PATH" | sed -e "s/SETTINGS_PATH=//g" ) 
-mkdir -p $SETTINGS/registry/auth
-docker run --entrypoint htpasswd registry:2 -Bbn default $PASSWD > $SETTINGS/registry/auth/htpasswd
-echo "Password is: $PASSWD"
